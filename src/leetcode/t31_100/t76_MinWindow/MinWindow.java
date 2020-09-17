cb8d2b4b196c290fe112ee9be4d2e98b9aaba6e7
admin <admin>
2020-09-17 18:58:45 +0800

    [feature/modify][all]: 优化目录，刷题

--- src/leetcode/t31_100/t76_MinWindow/MinWindow.java
+++ src/leetcode/t31_100/t76_MinWindow/MinWindow.java
@@ -1,5 +1,8 @@
 package leetcode.t31_100.t76_MinWindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 76. 最小覆盖子串
  *
@@ -18,11 +21,82 @@ package leetcode.t31_100.t76_MinWindow;
 public class MinWindow {
 
     public static void main(String[] args) {
-
+        System.out.println(minWindow("ADOBECODEBANC", "ABC")); // BANC
+        System.out.println(minWindow("ab", "b")); // b
+        System.out.println(minWindow("cabwefgewcwaefgcf", "cae")); // cwae
     }
 
+    /**
+     * 滑动窗口
+     *
+     * 时间复杂度：
+     * 空间复杂度：
+     */
     private static String minWindow(String s, String t) {
-        return null;
+        int sLen;
+        int tLen;
+
+        if (s == null || (sLen = s.length()) == 0 ||
+            t == null || (tLen = t.length()) == 0 ||
+            sLen < tLen) {
+            return "";
+        }
+
+        // 把子串的数据保存到 tMap 中，即(char, 出现的次数)
+        Map<Character, Integer> tMap = new HashMap<>();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+
+        int end = Integer.MAX_VALUE;
+        int start = -1;
+
+        int left = 0;
+        int right = 0;
+        Map<Character, Integer> windows = new HashMap<Character, Integer>();
+
+        // 遍历，窗口往右滑，当窗口右边界的值==字符串长度时结束
+        while (right < sLen) {
+            // 增大窗口
+            char cRight = s.charAt(right);
+            if (tMap.containsKey(cRight)) {
+                windows.put(cRight, windows.getOrDefault(cRight, 0) + 1);
+            }
+            right++;
+
+            // 遍历，右边界右滑已经保证当前窗口已经包含子串，这次是左边界向左移，求最小窗口值
+            while (right - left >= tLen && check(tMap, windows)) {
+                if (end > right - left) {
+//                    System.out.println("left: " + left + ", right: " + right);
+                    start = left;
+                    end = right - left;
+                }
+
+                // 缩小窗口
+                char cLeft = s.charAt(left);
+
+                // 如果包含左边界的值，当前窗口对应的键值对进行-1操作，然后求下一个满足要求的滑动窗口
+                if (tMap.containsKey(cLeft)) {
+                    windows.put(cLeft, windows.getOrDefault(cLeft, 0) - 1);
+                }
+                left++;
+            }
+        }
+
+        return start == -1 ? "" : s.substring(start, start + end);
     }
+
+    private static boolean check(Map<Character, Integer> tMap,
+                                 Map<Character, Integer> windows) {
+
+        for (Map.Entry<Character, Integer> next : tMap.entrySet()) {
+            if (windows.getOrDefault(next.getKey(), 0) < next.getValue()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
