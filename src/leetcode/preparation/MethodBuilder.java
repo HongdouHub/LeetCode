fea32da10dd10125d5b9753f159696fdb33ffaac
admin <admin>
2020-10-10 01:19:40 +0800

    [feature][877 && 887] : 刷题。

--- src/leetcode/preparation/MethodBuilder.java
+++ src/leetcode/preparation/MethodBuilder.java
@@ -7,6 +7,8 @@ public class MethodBuilder {
 
     private Class clazz;
 
+    private Object object;
+
     private String methodName;
 
     private Class<?>[] parameterTypes;
@@ -15,6 +17,10 @@ public class MethodBuilder {
         try {
             Method method = clazz.getDeclaredMethod(methodName, parameterTypes);
             method.setAccessible(true);
+
+            if (object != null) {
+                return method.invoke(object, parameters);
+            }
             return method.invoke(clazz, parameters);
         } catch (Exception e) {
             return e;
@@ -23,17 +29,24 @@ public class MethodBuilder {
 
     public MethodBuilder(Builder builder) {
         this.clazz = builder.clazz;
+        this.object = builder.object;
         this.methodName = builder.methodName;
         this.parameterTypes = builder.parameterTypes;
     }
 
     public static final class Builder {
         private Class clazz;
+        private Object object;
         private String methodName;
         private Class<?>[] parameterTypes;
 
         public Builder setClazz(Class clazz) {
             this.clazz = clazz;
+            return this;
+        }
+
+        public Builder setObject(Object object) {
+            this.object = object;
             return this;
         }
 
---
+++ src/leetcode/t887_SuperEggDrop/ISuperEggDrop.java
@@ -0,0 +1,13 @@
+package leetcode.t887_SuperEggDrop;
+
+public interface ISuperEggDrop {
+
+    /**
+     * 计算在 k 楼层，有 n 个鸡蛋下，最少的实验次数
+     *
+     * @param K 鸡蛋数
+     * @param N 楼层数
+     * @return 最少的实验次数
+     */
+    int superEggDrop(int K, int N);
+}
